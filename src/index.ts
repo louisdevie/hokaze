e3d9facc66b27f99375c83fc2760fdec8a296d89
Louis DEVIE <louisdevie.contact@gmail.com>
2024-08-23 23:09:55 +0200

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+/* LIBRARY USAGE */
 export { Service, service } from './service'
 export { string, boolean, number, /*ref,*/ array, enumeration, object } from './data/serialized'
+export { BasicAuth, BearerToken } from './auth'
 export { plainText } from './data/plainText'
 export { elz } from './locale'
+
+/* FOR PLUGINS */
+export type { ValidationResult } from './validation'
+export type { Likelihood } from './inference'
+export type { Checks } from './checks'
+export type { FieldRoleHints, ValueDescriptor } from './data/serialized'
+export type { ObjectDescriptor } from './data/serialized/object'
+export type { AuthScheme } from './auth'
+export { ValueMapper } from './mappers/serialized'
+export { AnyValue, type AnyValueOptions } from './data/serialized/base'
